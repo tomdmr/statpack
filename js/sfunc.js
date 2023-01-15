d8cc7af94dd2bc7b6af3cb3dfa808742d685bfc1
tom_(Laptop) <thd@mnet-online.de>
2023-01-15 10:17:32 +0100

    Added linear regression

--- js/sfunc.js
+++ js/sfunc.js
@@ -32,6 +32,7 @@ function Betinc(X,A,B) {
 	return A1/A
 }
 
+
 function t_Dist(t, nu) {
     X=t;
     df=nu;
@@ -57,6 +58,65 @@ function t_Dist(t, nu) {
 		tcdf=round(tcdf*100000)/100000;
 	}
     return tcdf;
+}
+
+function _subu (p) {
+	var y = -Math.log(4 * p * (1 - p));
+	var x = Math.sqrt(
+		y * (1.570796288
+		  + y * (.03706987906
+		  	+ y * (-.8364353589E-3
+			  + y *(-.2250947176E-3
+			  	+ y * (.6841218299E-5
+				  + y * (0.5824238515E-5
+					+ y * (-.104527497E-5
+					  + y * (.8360937017E-7
+						+ y * (-.3231081277E-8
+						  + y * (.3657763036E-10
+							+ y *.6936233982E-12)))))))))));
+	if (p>.5)
+                x = -x;
+	return x;
+}
+function _subt (n, p) {
+
+	if (p >= 1 || p <= 0) {
+		throw("Invalid p: p\n");
+	}
+
+	if (p == 0.5) {
+		return 0;
+	} else if (p < 0.5) {
+		return - _subt(n, 1 - p);
+	}
+
+	var u = _subu(p);
+	var u2 = Math.pow(u, 2);
+
+	var a = (u2 + 1) / 4;
+	var b = ((5 * u2 + 16) * u2 + 3) / 96;
+	var c = (((3 * u2 + 19) * u2 + 17) * u2 - 15) / 384;
+	var d = ((((79 * u2 + 776) * u2 + 1482) * u2 - 1920) * u2 - 945) 
+				/ 92160;
+	var e = (((((27 * u2 + 339) * u2 + 930) * u2 - 1782) * u2 - 765) * u2
+			+ 17955) / 368640;
+
+	var x = u * (1 + (a + (b + (c + (d + e / n) / n) / n) / n) / n);
+
+	if (n <= Math.pow(Math.log10(p), 2) + 3) {
+		var round;
+		do { 
+			var p1 = _subtprob(n, x);
+			var n1 = n + 1;
+			var delta = (p1 - p) 
+				/ Math.exp((n1 * Math.log(n1 / (n + x * x)) 
+					+ Math.log(n/n1/2/Math.PI) - 1 
+					+ (1/n1 - 1/n) / 6) / 2);
+			x += delta;
+			round = round_to_precision(delta, Math.abs(integer(log10(Math.abs(x))-4)));
+		} while ((x) && (round != 0));
+	}
+	return x;
 }
 
 
@@ -158,4 +218,60 @@ function random_bm(){
 
 function gaussian(mu, sigma){
     return sigma*random_bm() + mu;
+}
+/**
+   Compute linear regression of x and y. Return a,b, r (not r^2)
+ */
+function linearRegression(x, y){
+    let sXY =sX = sY = sX2 = sY2 = 0;
+    let n = x.length;
+    for(let i=0; i<n; i++){
+        sX  += x[i];
+        sX2 += x[i]*x[i];
+        sY  += y[i];
+        sXY += x[i]*y[i];
+        sY2 += y[i]*y[i];
+    }
+    //console.log(sX);
+    //console.log(sY);
+    let b = (n*sXY -sX*sY)/(n*sX2-sX*sX);
+    let a = (sY-b*sX)/n;
+    let r = (sXY-sX*sY/n)/Math.sqrt((sX2-sX*sX/n)*(sY2-sY*sY/n));
+    return {
+        n: n,
+        a: a,
+        b: b,
+        r: r,
+        sX: sX,
+        sX2: sX2,
+        sY: sY,
+        sY2: sY2,
+        sXY: sXY
+    };
+}
+function quadRegression(x, y){
+    let sXY =sX = sY = sX2 = sY2 = 0;
+    let n = x.length;
+    for(let i=0; i<n; i++){
+        let x2 = x[i]*x[i];
+        let x3 = x[i]*x2;
+        let x4 = x[i]*x3;
+
+        sX   += x[i];
+        sX2  += x2;
+        sX3  += x3;
+        sX4  += x4;
+
+        sXY  += x[i]*y[i];
+        sX2Y += x2  *y[i];
+    }
+    let qXX = sX2 - sX*sX/n;
+    let qXY = sXY -sX*sY/n;
+    let qX3 = sX3 - sX *sX2/n;
+    let qX4 = sX4 -sX2*sX2/n;
+    let qX2Y = sX2Y - sY * sX2/n;
+    let c = qXY*qX3*qX2Y*qX2/(qX3*qX3-qX2*qX4);
+    let b = (qXY-c*qX3)/qX2;
+    let a = (sY-b*sX-c*sX2)/n;
+    return { a:a, b:b, c:c};
 }
